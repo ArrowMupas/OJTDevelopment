2e037653627feec6f17add12d85810e6b75c2c1f
AriesSerene <140528488+AriesSerene@users.noreply.github.com>
2026-02-18 13:13:37 +0800

    1:13pm

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,35 +4,35 @@ export default function HomePage() {
   
 
   return (
-    <main className="p-4 max-w-md">
+    <main className="p-4 w-full">
       <h1 className="text-2xl font-bold mb-4">Home</h1>
+
 <div className="flex flex-col md:flex-row gap-4">
+
       <div className="card w-96 bg-base-100 card-md shadow-sm">
-  <div className="card-body">
-    <h2 className="card-title">Medium Card</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+      <div className="card-body">
+        <h2 className="card-title">21</h2>
+        <p>Today's Request</p>
+        <div className="justify-end card-actions">
+        </div>
+      </div>
+    </div>
+
+<div class="card w-96 bg-base-100 card-md shadow-sm">
+  <div class="card-body">
+    <h2 class="card-title">4</h2>
+    <p>Completed Request</p>
     <div class="justify-end card-actions">
-      <button class="btn btn-primary">Buy Now</button>
     </div>
   </div>
 </div>
 
 <div class="card w-96 bg-base-100 card-md shadow-sm">
   <div class="card-body">
-    <h2 class="card-title">Medium Card</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <h2 class="card-title">11</h2>
+    <p>Pending Request</p>
     <div class="justify-end card-actions">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
 
-<div class="card w-96 bg-base-100 card-md shadow-sm">
-  <div class="card-body">
-    <h2 class="card-title">Medium Card</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="justify-end card-actions">
-      <button class="btn btn-primary">Buy Now</button>
     </div>
   </div>
 </div>
