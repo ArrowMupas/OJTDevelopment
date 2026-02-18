0e87e5f03bd02dd3321c452eb80236c475674620
AriesSerene <140528488+AriesSerene@users.noreply.github.com>
2026-02-18 10:28:28 +0800

    18-10am

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <header className="bg-green-600 text-white p-4 shadow-md">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
+      <div className="max-w-7.5xl mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
         <img
               className="h-11 w-auto cursor-pointer"
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,42 @@
+import { House, ScrollText, UsersRound } from "lucide-react";
+
 export default function Sidebar() {
   return (
     <aside className="w-64 bg-gray-100 p-4 hidden md:block shadow-inner">
-      <h2 className="text-lg font-bold mb-4">Menu</h2>
+      {/* <h2 className="text-lg font-bold mb-4">Menu</h2> */}
       <ul className="space-y-2">
         <li>
           <a
             href="/"
-            className="block p-2 rounded hover:bg-blue-500 hover:text-white transition"
-          >
+            className="block p-2 rounded hover:bg-highlight hover:text-white transition"
+          ><House className="h-4 w-4 inline-block mr-2" />
             Home
           </a>
         </li>
         <li>
           <a
             href="#todos"
-            className="block p-2 rounded hover:bg-blue-500 hover:text-white transition"
-          >
+            className="block p-2 rounded hover:bg-highlight hover:text-white transition"
+          ><UsersRound className="h-4 w-4 inline-block mr-2" />
             Management
           </a>
         </li>
         <li>
           <a
             href="#about"
-            className="block p-2 rounded hover:bg-blue-500 hover:text-white transition"
-          >
+            className="block p-2 rounded hover:bg-highlight hover:text-white transition"
+          ><ScrollText className="h-4 w-4 inline-block mr-2" />
             Transactions
           </a>
         </li>
-        <li>
+        {/* <li>
           <a
             href="#contact"
-            className="block p-2 rounded hover:bg-blue-500 hover:text-white transition"
+            className="block p-2 rounded hover:bg-highlight hover:text-white transition"
           >
-            Contact
+            ------
           </a>
-        </li>
+        </li> */}
       </ul>
     </aside>
   );
