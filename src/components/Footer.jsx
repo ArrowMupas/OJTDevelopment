95481dc59da39da68b0cae438e1c3426fec47260
ArrowMupas <delmontearrow2004@gmail.com>
2026-02-18 09:41:34 +0800

    18/2 9:41 icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
+import { Star } from "lucide-react";
+
 export default function Footer() {
   return (
     <footer className="bg-gray-200 text-center text-gray-700 p-4 mt-auto">
+      <Star className="h-4 w-4" />
       <p>&copy; {new Date().getFullYear()} Todo App. All rights reserved.</p>
     </footer>
   );
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://jprkleohthakfrnncnon.supabase.co";
-const supabaseAnonKey = "sb_publishable_oeVAw8ism2OQr2pv2tIkvQ_wMhJYWYI";
+const supabaseUrl = "https://yelvewyjonvcyucwjcti.supabase.co";
+const supabaseAnonKey = "sb_publishable_JuLUkBQJ3BXc7IiU5gX_Vw_uyourgHT";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
