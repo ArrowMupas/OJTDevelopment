235c267daa26209c1d030acf45c0344bf64a3470
ArrowMupas <delmontearrow2004@gmail.com>
2026-02-20 10:30:28 +0800

    Contact submit logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,24 @@ import NotFoundPage from "./pages/NotFoundPage";
 import MainLayout from "./layouts/MainLayout";
 import Drivers from "./pages/Drivers";
 import DriverMonitoringPage from "./pages/home/DriverMonitoringPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/transactions" element={<TransactionsPage />} />
-        <Route path="/maintenance" element={<MaintenancePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/drivers" element={<Drivers />} />
-        <Route path="/drivermonitoring" element={<DriverMonitoringPage />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+          <Route path="/maintenance" element={<MaintenancePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/drivers" element={<Drivers />} />
+          <Route path="/drivermonitoring" element={<DriverMonitoringPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
