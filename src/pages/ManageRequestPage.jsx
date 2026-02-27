e974a5947ff654d5ba5084e24a90805d4224876c
ArrowMupas <delmontearrow2004@gmail.com>
2026-02-27 14:46:31 +0800

    Dashboard table sequence

--- src/pages/ManageRequestPage.jsx
+++ src/pages/ManageRequestPage.jsx
@@ -1,3 +1,4 @@
+import { format, parse } from "date-fns";
 import { supabase } from "../supabaseClient";
 import {
   Clipboard,
@@ -161,231 +162,149 @@ export default function ManageRequestsPage() {
           <table className="table table-zebra">
             <thead className="bg-green-600 text-white">
               <tr>
+                <th>Department</th>
                 <th>Passengers</th>
-                {/* <th>Request Number</th> */}
-                {/* <th>Timestamp</th> */}
-                {/* <th>Email Address</th> */}
-                {/* <th>Service Vehicle</th> */}
                 <th>Destination</th>
-                {/* <th>Time of departure</th>
-                <th>Date of departure</th>
-                <th>Purpose of travel</th>
-                <th>with:</th> */}
-                {/* <th>Requested by</th> */}
-                {/* <th>Duration of travel</th> */}
-                {/* <th>Other instructions</th> */}
-                <th>Contact No.</th>
-                {/* <th>Remarks</th> */}
+                <th>Date and Time.</th>
                 <th>Assigned Driver</th>
                 <th>Assigned vehicle</th>
-                <th>Plate No.</th>
                 <th>Status</th>
-                <th>Action</th>
-
-                {/* <th>Rating</th> */}
-                {/* <th>Reason for not completing</th> */}
+                <th></th>
               </tr>
             </thead>
             <tbody>
-              {requests.map((req) => (
-                <tr key={req.id}>
-                  <th className="flex flex-col">
-                    <span>{req.passengers}</span>
-                    <span className="text-xs text-gray-500 font-medium">
-                      {req.email}
-                    </span>
-                  </th>
-                  <td>{req.destination}</td>
-                  <td>{req.passenger_contact_number}</td>
+              {requests.map((req) => {
+                const date = req.departure_date;
+                const time = req.departure_time;
 
-                  {/* DRIVER SELECT */}
-                  <td>
-                    <select
-                      className="select select-ghost"
-                      value={req.driver_id || ""}
-                      onChange={(e) =>
-                        updateAssignedDriver(req.id, Number(e.target.value))
-                      }
-                    >
-                      <option value="">Unassigned</option>
+                const parsedDateTime = parse(
+                  `${date} ${time}`,
+                  "yyyy-MM-dd HH:mm:ss",
+                  new Date(),
+                );
 
-                      {drivers.map((driver) => (
-                        <option key={driver.id} value={driver.id}>
-                          {driver.first_name} {driver.last_name}
+                const formattedDateTime = format(
+                  parsedDateTime,
+                  "MMM. d, yyyy hh:mm a",
+                );
+
+                return (
+                  <tr key={req.id}>
+                    <th>{req.department}</th>
+
+                    <td className="">
+                      <span className="font-bold capitalize">
+                        {req.passengers}
+                      </span>
+                      <br />
+                      <span className="text-xs text-gray-500 font-medium">
+                        {req.email}
+                      </span>
+                      <br />
+                      <span className="text-xs  font-medium">
+                        {req.passenger_contact_number}
+                      </span>
+                    </td>
+
+                    <td>{req.destination}</td>
+
+                    <td className="">
+                      <span className="text-sm">
+                        {format(formattedDateTime, "MMM. d, yyyy")}
+                      </span>
+                      <br />
+                      <span className="text-xs ">
+                        {format(formattedDateTime, "hh:mm a")}
+                      </span>
+                    </td>
+
+                    {/* DRIVER SELECT */}
+                    <td>
+                      <select
+                        className="select select-ghost"
+                        value={req.driver_id || ""}
+                        onChange={(e) =>
+                          updateAssignedDriver(req.id, Number(e.target.value))
+                        }
+                      >
+                        <option value="">Unassigned</option>
+
+                        {drivers.map((driver) => (
+                          <option key={driver.id} value={driver.id}>
+                            {driver.first_name} {driver.last_name}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
+
+                    {/* VEHICLE SELECT */}
+                    <td>
+                      <div className="flex flex-col gap-2">
+                        <select
+                          className="select "
+                          value={req.vehicle_id || ""}
+                          onChange={(e) =>
+                            updateAssignedVehicle(
+                              req.id,
+                              Number(e.target.value),
+                            )
+                          }
+                        >
+                          <option value="">Unassigned</option>
+                          {vehicles.map((vehicle) => (
+                            <option key={vehicle.id} value={vehicle.id}>
+                              {vehicle.name}
+                            </option>
+                          ))}
+                        </select>
+                        <div className="badge badge-dash badge-primary">
+                          {vehicles.find((v) => v.id === req.vehicle_id)
+                            ?.plate_number ?? "N/A"}
+                        </div>
+                      </div>
+                    </td>
+
+                    {/* STATUS */}
+                    <td>
+                      <select
+                        className={`select  ${
+                          req.status === "Completed"
+                            ? " text-green-500 select-success"
+                            : req.status === "Cancelled" &&
+                              "select-error  text-red-500 "
+                        }`}
+                        value={req.status || ""}
+                        onChange={(e) => updateStatus(req.id, e.target.value)}
+                      >
+                        <option value="" className="text-black">
+                          Pending
                         </option>
-                      ))}
-                    </select>
-                  </td>
-
-                  {/* VEHICLE SELECT */}
-                  <td>
-                    <select
-                      className="select select-ghost"
-                      value={req.vehicle_id || ""}
-                      onChange={(e) =>
-                        updateAssignedVehicle(req.id, Number(e.target.value))
-                      }
-                    >
-                      <option value="">Unassigned</option>
-
-                      {vehicles.map((vehicle) => (
-                        <option key={vehicle.id} value={vehicle.id}>
-                          {vehicle.name}
+                        <option value="Completed" className="text-green-500 ">
+                          Completed
                         </option>
-                      ))}
-                    </select>
-                  </td>
+                        <option value="Cancelled" className="text-red-500">
+                          Cancelled
+                        </option>
+                      </select>
+                    </td>
 
-                  <td>
-                    <div className="badge badge-dash badge-primary">
-                      {vehicles.find((v) => v.id === req.vehicle_id)
-                        ?.plate_number ?? "N/A"}
-                    </div>
-                  </td>
-
-                  {/* STATUS */}
-                  <td>
-                    <select
-                      className={`select select-ghost ${
-                        req.status === "Completed"
-                          ? "bg-green-200 text-green-800"
-                          : req.status === "Cancelled"
-                            ? "bg-red-200 text-red-800"
-                            : "bg-yellow-100 text-yellow-800"
-                      }`}
-                      value={req.status || ""}
-                      onChange={(e) => updateStatus(req.id, e.target.value)}
-                    >
-                      <option value="">Pending</option>
-                      <option value="Completed">Completed</option>
-                      <option value="Cancelled">Cancelled</option>
-                    </select>
-                  </td>
-
-                  {/* ACTION */}
-                  <td>
-                    <Link to="/moreinfo">
-                      <button className="btn btn-square">
-                        <Ellipsis className="h-4 w-6" />
-                      </button>
-                    </Link>
-                  </td>
-                </tr>
-              ))}
+                    {/* ACTION */}
+                    <td>
+                      <Link to="/moreinfo">
+                        <button className="btn btn-square">
+                          <Ellipsis className="h-4 w-6" />
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+              )
             </tbody>
             <tfoot></tfoot>
           </table>
         </div>
       </div>
-
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box w-11/12 max-w-2xl max-h-[90vh] border-3 border-doubled border-gray-200 rounded-lg">
-          <h3 className="font-bold text-3xl flex items-center gap-2">
-            <Info className="h-9 w-9" />
-            Activity Information
-          </h3>
-          <div className="mt-6">
-            <div className="md:gap-12 text-sm">
-              <div className="border border-gray-300 rounded-lg p-4 relative z-0 w-full mb-4 group">
-                <div className="md:gap-12 md:col-2">
-                  <h2 className="text-2xl font-bold mb-2 border-b-2 border-black pb-2">
-                    Request Identification
-                  </h2>
-
-                  <legend className="fieldset-legendc font-bold">
-                    Request Number:
-                  </legend>
-                  <p className="mb-2">[Request Number here.]</p>
-                  <legend className="fieldset-legendc font-bold">
-                    Timestamp:
-                  </legend>
-                  <p className="mb-2">[Timestamp here.]</p>
-                  <legend className="fieldset-legendc font-bold">
-                    Requested by:
-                  </legend>
-                  <p className="mb-2">[Requested by here.]</p>
-                </div>
-              </div>
-
-              <div className="border border-gray-300 rounded-lg p-4 relative z-0 w-full mb-4 group">
-                <div className="md:gap-12 md:col-2">
-                  <h2 className="text-2xl font-bold mb-2 border-b-2 border-black pb-2">
-                    Travel Schedule
-                  </h2>
-                  <legend className="fieldset-legendc font-bold">
-                    Date of Departure:
-                  </legend>
-                  <p className="mb-2">[Date of Departure here.]</p>
-                  <legend className="fieldset-legendc font-bold">
-                    Time of Departure:
-                  </legend>
-                  <p className="mb-2">[Time of Departure here.]</p>
-                  <legend className="fieldset-legendc font-bold">
-                    Duration of Travel:
-                  </legend>
-                  <p className="mb-2">[Duration of Travel here.]</p>
-                </div>
-              </div>
-
-              <div className="border border-gray-300 rounded-lg p-4 relative z-0 w-full mb-4 group">
-                <div className="md:gap-12 md:col-2">
-                  <h2 className="text-2xl font-bold mb-2 border-b-2 border-black pb-2">
-                    Vehicle Information
-                  </h2>
-                  <legend className="fieldset-legendc font-bold">
-                    Service Vehicle:
-                  </legend>
-                  <p className="mb-2">[Service Vehicle here.]</p>
-                  <legend className="fieldset-legendc font-bold">
-                    Held Items:
-                  </legend>
-                  <p className="mb-2">[Held Items here.]</p>
-                </div>
-              </div>
-
-              <div className="border border-gray-300 rounded-lg p-4 relative z-0 w-full mb-4 group">
-                <div className="md:gap-12 md:col-2">
-                  <h2 className="text-2xl font-bold mb-2 border-b-2 border-black pb-2">
-                    Travel Information
-                  </h2>
-                  <legend className="fieldset-legendc font-bold">
-                    Purpose of Travel:
-                  </legend>
-                  <p className="mb-2">[Purpose of Travel here.]</p>
-                  <legend className="fieldset-legendc font-bold">
-                    Other Instructions:
-                  </legend>
-                  <p className="mb-2">[Other Instructions here.]</p>
-                </div>
-              </div>
-
-              <div className="border border-gray-300 rounded-lg p-4 relative z-0 w-full mb-4 group">
-                <div className="md:gap-12 md:col-2">
-                  <h2 className="text-2xl font-bold mb-2 border-b-2 border-black pb-2">
-                    Feedback Details
-                  </h2>
-                  <legend className="fieldset-legendc font-bold">
-                    Remarks:
-                  </legend>
-                  <p className="mb-2">[Remarks here.]</p>
-                  <legend className="fieldset-legendc font-bold">
-                    Ratings:
-                  </legend>
-                  <p className="mb-2">[Ratings here.]</p>
-                  <legend className="fieldset-legendc font-bold">
-                    Reason for not completing:
-                  </legend>
-                  <p className="mb-2">[Reason for not completing here.]</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
     </main>
   );
 }
